237d572f71a63accbac34703617f25ba0f4c3b0a
Ashwinpk <43211843+Ashwinpk@users.noreply.github.com>
2019-04-25 02:27:50 +0530

    Add files via upload

---
+++ mac_changer.py
@@ -0,0 +1,60 @@
+import subprocess
+import optparse
+import re
+
+def print_my_logo():
+    print("")
+    print("--------------------------------------------------------------")
+    print("This Script Is Done By Ashwin Pk!")
+    print("--------------------------------------------------------------")
+    print("--------------------------------------------------------------")
+    print("You Can Learn How To Do Scripts Like This On www.frankeey.com")
+    print("--------------------------------------------------------------")
+    print("")
+
+
+def get_options():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Used to select interface")
+    parser.add_option("-m", "--mac", dest="new_mac", help="Used to select mac")
+    (options, arguments) =  parser.parse_args()
+    if not options.interface:
+        parser.error("interface is not specified! use --help to get more info")
+    elif not options.new_mac:
+        parser.error("Mac Id is not specified! use --help to get more info")
+    else:
+        return options
+
+
+def change_mac(interface, new_mac):
+    print("Changing Mac Address " + interface + " To " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+def get_mac_address(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    filter_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if filter_result:
+        return filter_result.group(0)
+    else:
+        print("MAC Address Not Found")
+
+
+print_my_logo()
+options = get_options()
+
+mac_address_filter_results = get_mac_address(options.interface)
+print("Current MAC = " + str(mac_address_filter_results))
+
+change_mac(options.interface, options.new_mac)
+
+mac_address_filter_results = get_mac_address(options.interface)
+if mac_address_filter_results == options.new_mac:
+    print("Mac Address Changed To: " + mac_address_filter_results)
+else:
+    print("Mac Address Is Not Changed")
+
+
+
